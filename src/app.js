e54c7fcb5bfa201d45b80a33a4bd148aea0e36df
ICE <alvesiasaiah@gmail.com>
2025-02-27 20:41:01 +0800

    added cors codes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const userRoutes = require('./routes/userRoutes.js');
 const uploadRoutes = require('./routes/uploadRoutes.js');
 const path = require("path");
 
+
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,12 @@ const app = express();
 //  Apply Middleware First
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(cors({
+  origin: "https://app-backend-server-fivnp53pp-iasaiah-alves-projects.vercel.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 //  Serve static files
 app.use(express.static(path.join(__dirname, "../public")));
